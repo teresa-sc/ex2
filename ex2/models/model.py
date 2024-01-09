4494aea038c98fe372fa97745e566588500dacc8
teresa <tksc@dtu.dk>
2024-01-09 15:37:54 +0100

    rewrite to lightning

--- ex2/models/model.py
+++ ex2/models/model.py
@@ -1,17 +1,40 @@
-from torch import nn
+from torch import nn, optim
 import hydra
 import torch
 from omegaconf import OmegaConf
+from pytorch_lightning import LightningModule
 
 cfg = OmegaConf.load('ex2/config/config_model.yaml')
+cfg_train = OmegaConf.load('ex2/config/config_training.yaml')
 torch.manual_seed(cfg.seed)
-myawesomemodel = nn.Sequential(
-    nn.Conv2d(1, cfg.num_filters_lay1, cfg.filter_size),  # [B, 1, 28, 28] -> [B, 32, 26, 26]
-    nn.LeakyReLU(),
-    nn.Conv2d(cfg.num_filters_lay1, cfg.num_filters_lay2, cfg.filter_size), # [B, 32, 26, 26] -> [B, 64, 24, 24]
-    nn.LeakyReLU(),
-    nn.MaxPool2d(2),      # [B, 64, 24, 24] -> [B, 64, 12, 12]
-    nn.Flatten(),        # [B, 64, 12, 12] -> [B, 64 * 12 * 12]
-    nn.Linear(cfg.num_filters_lay2 * cfg.flatten_dim * cfg.flatten_dim, cfg.out_dim), # [B, 64 * 12 * 12] -> [B, 10]
-)
 
+class MyAwesomeModel(LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+                    nn.Conv2d(1, cfg.num_filters_lay1, cfg.filter_size),  # [B, 1, 28, 28] -> [B, 32, 26, 26]
+                    nn.LeakyReLU(),
+                    nn.Conv2d(cfg.num_filters_lay1, cfg.num_filters_lay2, cfg.filter_size), # [B, 32, 26, 26] -> [B, 64, 24, 24]
+                    nn.LeakyReLU(),
+                    nn.MaxPool2d(2),      # [B, 64, 24, 24] -> [B, 64, 12, 12]
+                    nn.Flatten(),        # [B, 64, 12, 12] -> [B, 64 * 12 * 12]
+                    nn.Linear(cfg.num_filters_lay2 * cfg.flatten_dim * cfg.flatten_dim, cfg.out_dim), # [B, 64 * 12 * 12] -> [B, 10]
+                    )
+
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=cfg_train.lr)
+
+    def forward(self, x):
+        return self.model(x)
+    
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_pred = self.model(x)
+        loss = self.loss_fn(y_pred, y)
+        self.log('train_loss', loss)
+        acc = (y == y_pred.argmax(dim=-1)).float().mean()
+        self.log('train_acc', acc)
+        return loss
+    
+    def configure_optimizers(self):
+        return self.optimizer
--- ex2/train_model.py
+++ ex2/train_model.py
@@ -1,35 +1,37 @@
 import torch
 import os
 import hydra
+from pytorch_lightning import Trainer
+import pytorch_lightning as pl
+import wandb
 from torch import nn
-from models.model import myawesomemodel
+from models.model import MyAwesomeModel
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 import logging
 log = logging.getLogger(__name__)
 
+
 @hydra.main(config_path="config", config_name="config_training")
 def train(cfg):
     torch.manual_seed(cfg.seed)
     log.info(f"Training with config: {cfg}")
-    model = myawesomemodel.to(device)
-    train_set = torch.load(cfg.dataset_path)
+
+    train_set = torch.load(os.path.join(cfg.dataset_path, "train_images.pt"))
     train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=cfg.batch_size)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=cfg.lr)
-    loss_fn = nn.CrossEntropyLoss()
+    test_set = torch.load(os.path.join(cfg.dataset_path, "test_images.pt"))
+    test_dataloader = torch.utils.data.DataLoader(test_set, batch_size=cfg.batch_size)
 
-    for epoch in range(cfg.num_epochs):
-        for batch in train_dataloader:
-            optimizer.zero_grad()
-            x, y = batch
-            x = x.to(device)
-            y = y.to(device)
-            y_pred = model(x)
-            loss = loss_fn(y_pred, y)
-            loss.backward()
-            optimizer.step()
-        log.info(f"Epoch {epoch} Loss {loss}")
+
+    model = MyAwesomeModel()
+    trainer = Trainer(max_epochs=cfg.num_epochs,
+                      default_root_dir=os.getcwd(),
+                      logger=pl.loggers.WandbLogger(project="mnist_mlops"),
+                      limit_train_batches=0.2,
+                      )
+    trainer.fit(model, train_dataloader)
+    trainer.test(model, test_dataloader)
 
     torch.save(model, f"{os.getcwd()}/trained_model.pt")
     log.info("Model saved")
